ad42a573a372c87687d4384a9ec2c8a832ba279a
bmarapareddy <bmarapareddy@paypal.com>
2019-12-17 11:47:36 -0800

    Creation of radio button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      country: 'australia'
+    };
+
+    this.onRadioChange = this.onRadioChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onRadioChange = (e) => {
+    this.setState({
+      country: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmit}>
+          <strong>Select country:</strong>
+
+          <ul>
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="india"
+                  checked={this.state.country === "india"}
+                  onChange={this.onRadioChange}
+                />
+                <span>India</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="australia"
+                  checked={this.state.country === "australia"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Australia</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="england"
+                  checked={this.state.country === "england"}
+                  onChange={this.onRadioChange}
+                />
+                <span>England</span>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input
+                  type="radio"
+                  value="spain"
+                  checked={this.state.country === "spain"}
+                  onChange={this.onRadioChange}
+                />
+                <span>Spain</span>
+              </label>
+            </li>
+
+          </ul>
+
+          <button type="submit">Choose Country</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
